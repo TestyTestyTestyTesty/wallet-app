901e4e27ae4046102146bb40b10e2b178feef8ca
ST <31928306+TestyTestyTestyTesty@users.noreply.github.com>
2019-01-31 20:41:54 +0100

    dodanie material design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
+
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Tymson zapraszam do zabawy
-          </p>
-        </header>
+        <Button variant="contained" color="primary">
+      Hello World
+    </Button>
       </div>
     );
   }
